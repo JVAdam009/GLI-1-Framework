ebd68e26042248826987707b0ab5d83393f94ae9
Josh <Harlock@duck.com>
2022-11-29 21:06:51 -0500

    Spawn Manager and Object Pool

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,22 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField]private NavMeshAgent _agent;
-    [SerializeField]private Transform _EndPoint;
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _agent.destination = _EndPoint.position;
+        SpawningManager.Instance.RequestNewDestination(_agent);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
